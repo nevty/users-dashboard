c469cf7eadc183f9087027008e4864e8de2f0dbf
nevty <theacutemace@gmail.com>
2023-12-13 12:52:20 +0300

    main screen overflow

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { MainDashboardPage } from './pages';
 
 function App() {
   return (
-    <div className="w-screen h-screen">
+    <div className="w-screen h-screen overflow-hidden">
       <MainDashboardPage />
     </div>
   );
