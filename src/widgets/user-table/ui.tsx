ecaeadfdd033d88771fba8317bb2cc010749a7cc
nevty <theacutemace@gmail.com>
2023-12-14 16:57:12 +0300

    widgets/user-table/ui: - selected user state - drawer with user transaction history - open drawer on selected user

--- src/widgets/user-table/ui.tsx
+++ src/widgets/user-table/ui.tsx
@@ -1,6 +1,15 @@
+import Drawer from 'rc-drawer';
 import { useMemo, useState } from 'react';
 import { useQuery } from 'react-query';
-import { Search, Table, Pagination, SortDirection } from '@shared/ui';
+import { UserTransactionHistory } from '@features/user-transaction-history';
+import { User } from '@entities/user';
+import {
+  Search,
+  Table,
+  Pagination,
+  SortDirection,
+  CloseIcon,
+} from '@shared/ui';
 import { fetchUserList } from './api';
 import { createUserColumns } from './lib';
 
@@ -8,8 +17,9 @@ export const UserTable = () => {
   const [currentPage, onPageChange] = useState(1);
   const [searchQuery, onSearch] = useState('');
   const [tokenSort, onTokenSort] = useState<SortDirection>(null);
+  const [selectedUser, selectUser] = useState<User | null>(null);
 
-  const columns = useMemo(() => createUserColumns(onTokenSort), [onTokenSort]);
+  const columns = useMemo(() => createUserColumns(onTokenSort, selectUser), []);
 
   const { data, isError, isFetching } = useQuery({
     queryKey: ['user-list', currentPage, searchQuery, tokenSort],
@@ -43,6 +53,31 @@ export const UserTable = () => {
           isDisabled={isFetching}
         />
       </div>
+      <Drawer
+        open={Boolean(selectedUser)}
+        onClose={() => selectUser(null)}
+        classNames={{
+          wrapper: '!w-full sm:!w-1/2 lg:!w-2/5',
+        }}
+      >
+        {selectedUser && (
+          <div className="flex flex-col px-4 py-8 sm:px-5 sm:py-14 gap-3">
+            <div className="flex justify-between">
+              <span className="text-lg font-semibold">
+                {selectedUser.email}
+              </span>
+              <button
+                type="button"
+                className="bg-transparent px-2 py-1"
+                onClick={() => selectUser(null)}
+              >
+                <CloseIcon />
+              </button>
+            </div>
+            <UserTransactionHistory user={selectedUser} />
+          </div>
+        )}
+      </Drawer>
     </>
   );
 };
